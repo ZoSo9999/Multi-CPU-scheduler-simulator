fb2c373067ccd1f8056631eeb4bef25a2b9c584f
ZoSo9999 <81162112+ZoSo9999@users.noreply.github.com>
2021-06-28 12:17:20 +0200

    theoretic approach
    data structures comprehension and idea of the final project (use of array for running processes)

---
+++ fake_os.h
@@ -0,0 +1,30 @@
+#include "fake_process.h"
+#include "linked_list.h"
+#pragma once
+
+#define MAX_RUNNING 4
+
+
+typedef struct {
+  ListItem list;
+  int pid;
+  ListHead events;
+} FakePCB;
+
+struct FakeOS;
+typedef void (*ScheduleFn)(struct FakeOS* os, void* args);
+
+typedef struct FakeOS{
+  FakePCB* running[MAX_RUNNING];
+  ListHead ready;
+  ListHead waiting;
+  int timer;
+  ScheduleFn schedule_fn;
+  void* schedule_args;
+
+  ListHead processes;
+} FakeOS;
+
+void FakeOS_init(FakeOS* os);
+void FakeOS_simStep(FakeOS* os);
+void FakeOS_destroy(FakeOS* os);
